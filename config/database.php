a6b0de175f5a5da47eb061f2654657bad37a2652
Børge Warvik <bowarv@gmail.com>
2019-03-07 14:12:54 +0100

    Trying to get sqlite in memory working'

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
 
         'sqlite_testing' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => ':memory:', //env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
